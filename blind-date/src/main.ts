afefa06cb4321cab2e3f73592ba814912d7f48a3
Sourav <souravsaha101@gmail.com>
2020-12-19 22:11:30 +0530

    firebase

--- blind-date/src/main.ts
+++ blind-date/src/main.ts
@@ -4,8 +4,10 @@ import router from "./router";
 import store from "./store";
 import "./assets/css/styles.css";
 import "./assets/css/bootstrap.css";
+import { firestorePlugin } from 'vuefire'
 
 createApp(App)
+.use(firestorePlugin)
   .use(store)
   .use(router)
   .mount("#app");
